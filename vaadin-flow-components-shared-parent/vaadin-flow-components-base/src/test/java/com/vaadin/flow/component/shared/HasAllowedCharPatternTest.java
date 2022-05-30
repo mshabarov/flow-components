a8f70046512c69c5df75dbae204fd57f02950007
web-padawan <iamkulykov@gmail.com>
2022-05-30 10:40:35 +0300

    feat: add HasAllowedCharPattern interface

---
+++ vaadin-flow-components-shared-parent/vaadin-flow-components-base/src/test/java/com/vaadin/flow/component/shared/HasAllowedCharPatternTest.java
@@ -0,0 +1,39 @@
+package com.vaadin.flow.component.shared;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+
+public class HasAllowedCharPatternTest {
+
+    private TestComponent component;
+
+    @Before
+    public void setup() {
+        component = new TestComponent();
+    }
+
+    @Test
+    public void initialValue() {
+        Assert.assertFalse(component.getAllowedCharPattern(), "");
+    }
+
+    @Test
+    public void changeValue() {
+        component.setAllowedCharPattern("[-+\\d]");
+        Assert.assertEqual(component.getElement()
+                .getProperty("allowedCharPattern", "[-+\\d]"));
+
+        component.setAllowedCharPattern(null);
+        Assert.assertEqual(
+                component.getElement().getProperty("allowedCharPattern", ""));
+    }
+
+    @Tag("test")
+    private static class TestComponent extends Component
+            implements HasAllowedCharPattern {
+    }
+}
